67682c2536cc34fa657d709d05290a30977cddf0
Darren Starr <darren@nocturnal.no>
2017-06-13 09:26:52 +0200

    Created unit test for creating guest users with lifetimes of less than 4 hours.

--- CiscoISE.Tests/GuestUsers.cs
+++ CiscoISE.Tests/GuestUsers.cs
@@ -82,6 +82,27 @@ namespace CiscoISE.Tests
             Assert.IsTrue(deleteTestOk, "Cannot delete user");
         }
 
+        [TestMethod]
+        public async Task CreateandDeleteUser4Hours()
+        {
+            var connection = new ISEConnection(baseUrl, username, password);
+            var developerConnection = new ISEConnection(baseUrl, developerUsername, developerPassword);
+
+            var deleteOk = await DeleteUserIfExist(connection, "dummyuser");
+            Assert.IsTrue(deleteOk, "User already exists and cannot be delete");
+
+            var createOk = await CreateValidNewUser4Hours(developerConnection, connection, "dummyuser");
+            Assert.IsTrue(createOk, "Failed to create user");
+
+            Trace.WriteLine("Getting record of new user");
+            var user = await CiscoISE.GuestUsers.Get(connection, "dummyuser");
+            Assert.IsNotNull(user, "Can't retreive user following creation");
+
+            Trace.WriteLine("Deleting user");
+            var deleteTestOk = await CiscoISE.GuestUsers.Delete(connection, user);
+            Assert.IsTrue(deleteTestOk, "Cannot delete user");
+        }
+
         private async Task<bool> DeleteUserIfExist(ISEConnection connection, string username)
         {
             Trace.WriteLine("Retrieving user from ISE if it already exist");
@@ -128,6 +149,47 @@ namespace CiscoISE.Tests
                     {
                         ValidDays = 100,
                         Location = "San Jose"
+                    }
+                });
+        }
+
+        private async Task<bool> CreateValidNewUser4Hours(
+            ISEConnection developerConnection,
+            ISEConnection connection,
+            string username)
+        {
+            Trace.WriteLine("Getting Sponsor Portal ID");
+            var portals = await CiscoISE.Portals.Get(developerConnection);
+            Assert.IsNotNull(portals, "Failed to get portals");
+
+            var sponsorPortal = portals.Where(x => x.Name == "Sponsor Portal (default)").FirstOrDefault();
+            Assert.IsNotNull(sponsorPortal, "Failed to find sponsor portal in list of portals");
+
+            var fromDate = DateTime.Now;
+            var toDate = fromDate.AddHours(4);
+
+            Trace.WriteLine("Creating new user");
+            return await CiscoISE.GuestUsers.Create(
+                connection,
+                new GuestUserViewModel
+                {
+                    GuestType = "Contractor (default)",
+                    PortalId = sponsorPortal.Id.ToString(),
+                    GuestInfo = new GuestInfoViewModel
+                    {
+                        Username = username,
+                        Password = "Minions12345",
+                        FirstName = "dummy",
+                        LastName = "user",
+                        EmailAddress = "dummyuser@funkychicken.org",
+                        Enabled = true
+                    },
+                    GuestAccessInfo = new GuestAccessInfoViewModel
+                    {
+                        ValidDays = 1,
+                        Location = "San Jose",
+                        FromDate = fromDate,
+                        ToDate = toDate
                     }
                 });
         }
